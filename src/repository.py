31d2ae68c100d71e3177a55ab7773094aa82e4a5
Jerry Yu <983951558@qq.com>
2018-10-20 17:51:57 +0800

    完成周、月k线图

--- src/repository.py
+++ src/repository.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+# coding=utf-8
+
 import os
 import sqlite3
 
@@ -44,12 +46,53 @@ class SQLiteUtil(object):
     def select_group_by_week():
         db_conn = sqlite3.connect('../data/candle.db')
         cursor = db_conn.cursor()
-        rows = cursor.execute(
+        cursor.execute(
             "SELECT week_flag,min(min_price),max(max_price) FROM tb_candle GROUP BY week_flag ORDER BY week_flag")
-        for row in rows:
-            print row
+        week_data = cursor.fetchall()
+        cursor.execute(
+            "SELECT week_flag, first_price FROM tb_candle GROUP BY week_flag HAVING min(ID) ORDER BY ID ")
+        first_price_data = cursor.fetchall()
+        cursor.execute(
+            "SELECT week_flag, last_price FROM tb_candle GROUP BY week_flag HAVING max(ID) ORDER BY ID ")
+        last_price_data = cursor.fetchall()
         db_conn.commit()
         db_conn.close()
+        index = 0
+        result = []
+        # print first_price_data[index][1]
+        for row in week_data:
+            obj = {"week_flag": row[0], "min_price": row[1], "max_price": row[2],
+                   "first_price": first_price_data[index][1], "last_price": last_price_data[index][1]}
+            result.append(obj)
+            index = index + 1
+        print result
+        return result
+
+    @staticmethod
+    def select_group_by_moon():
+        db_conn = sqlite3.connect('../data/candle.db')
+        cursor = db_conn.cursor()
+        cursor.execute(
+            "SELECT moon_flag,min(min_price),max(max_price) FROM tb_candle GROUP BY moon_flag ORDER BY moon_flag")
+        moon_data = cursor.fetchall()
+        cursor.execute(
+            "SELECT moon_flag, first_price FROM tb_candle GROUP BY moon_flag HAVING min(ID) ORDER BY ID ")
+        first_price_data = cursor.fetchall()
+        cursor.execute(
+            "SELECT moon_flag, last_price FROM tb_candle GROUP BY moon_flag HAVING max(ID) ORDER BY ID ")
+        last_price_data = cursor.fetchall()
+        db_conn.commit()
+        db_conn.close()
+        index = 0
+        result = []
+        # print first_price_data[index][1]
+        for row in moon_data:
+            obj = {"moon_flag": row[0], "min_price": row[1], "max_price": row[2],
+                   "first_price": first_price_data[index][1], "last_price": last_price_data[index][1]}
+            result.append(obj)
+            index = index + 1
+        print result
+        return result
 
     @staticmethod
     def remove_all():
--- src/ui.py
+++ src/ui.py
@@ -1,24 +1,37 @@
 #!/usr/bin/python
-# -*- coding: UTF-8 -*-
+# coding=utf-8
+
 
 import os
 
 import tkinter
 
 from data_handler import Cleaner
+from src.data_show import DataShow
 from src.repository import SQLiteUtil
 
 
+def change_progress(txt):
+    progress_txt.set(txt)
+    # 一定要执行，否则只会等scan_file执行后才会更新
+    progress.update()
+
+
 def scan_file():
+    change_progress("正在扫描...")
     root_dir = text_input.get()
     print "dir is:" + root_dir
     if root_dir is None:
         return
-    root_dir = "/Users/jerryyu/Downloads/橡胶价格数据包"
+    # root_dir = "/Users/jerryyu/Downloads/橡胶价格数据包"
     sub_dirs = os.listdir(root_dir)
     sub_dirs.sort()
     current_time = 0
+    change_progress("正在清洗...")
+
     for sub_dir in sub_dirs:
+        if not sub_dir.startswith("1"):
+            continue
         files = os.listdir(root_dir + "/" + sub_dir)
         files.sort()
         for file in files:
@@ -27,15 +40,25 @@ def scan_file():
                 candle = Cleaner.clean(f)
                 SQLiteUtil.insert(candle, current_time)
                 current_time = current_time + 1
-
-    # SQLiteUtil.remove_all()
+    change_progress("创建视图...")
+    DataShow.create_view()
+    SQLiteUtil.remove_all()
+    change_progress("完成！")
 
 
 root = tkinter.Tk()
+root.title('数据可视化')
+root.geometry('500x200')
+tkinter.Label(root, text='目录：').place(x=100, y=5)
 text_input = tkinter.Entry(root)
 text_input.pack()
-btn1 = tkinter.Button(root, text='打开csv文件', font=("宋体", 10, 'bold'), width=6, height=2, command=scan_file)
+btn1 = tkinter.Button(root, text='开始扫描目录', font=("宋体", 10, 'bold'), width=10, height=2, command=scan_file)
 
 btn1.pack()
+progress_txt = tkinter.StringVar()
+progress = tkinter.Entry(root, textvariable=progress_txt, bd=0)
+progress['state'] = 'readonly'
+
+progress.pack()
 
 root.mainloop()
